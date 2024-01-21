2babf47b5a4a31e646a43e0c7bbb6c996bcd5c30
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-01-21 18:26:55 -0300

    Script de login pronto

---
+++ login.js
@@ -0,0 +1,17 @@
+form = document.querySelector("form");
+email = form.querySelector("#email");
+password = form.querySelector("#password");
+
+form.onsubmit = e => {
+  e.preventDefault();
+  if(email.value != "pythonimpressionador@gmail.com") {
+    alert("Esse E-mail não está cadastrado!");
+    email.value = "";
+    password.value = "";
+  } else if(password.value != "python2024") {
+    alert("A senha está incorreta!");
+    password.value = "";
+  } else {
+    window.location.href = window.origin + "/product-registration.html"
+  }
+}
