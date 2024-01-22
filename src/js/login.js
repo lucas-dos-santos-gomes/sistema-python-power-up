d75d7ed025198e7649aab824e843d838d62f11ed
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2024-01-21 21:40:12 -0300

    Bug de navegação corrigido

--- src/js/login.js
+++ src/js/login.js
@@ -12,6 +12,6 @@ form.onsubmit = e => {
     alert("A senha está incorreta!");
     password.value = "";
   } else {
-    window.location.href = window.origin + "/product-registration.html"
+    window.location.href = window.location.href + "/product-registration.html"
   }
 }
